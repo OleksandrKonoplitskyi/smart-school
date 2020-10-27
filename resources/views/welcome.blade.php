eab1db595a89c843c91b9fe610cc2e46443d72a4
Oleksandr Konoplitsky <konoplitsky91@gmail.com>
2020-10-27 14:10:13 +0200

    2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Smart -school
+                    Smart - school
                 </div>
 
               
